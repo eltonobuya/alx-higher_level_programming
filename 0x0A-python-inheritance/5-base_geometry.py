a408d91faacdb06382c0822f752f7b39176b09ed
EltonObuya <99792526+eltonobuya@users.noreply.github.com>
2022-09-27 11:46:19 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Empty Geometry Class """
+
+
+class BaseGeometry:
+    """ Empty class """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Empty Geometry Class """
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
